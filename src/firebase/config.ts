f67a7a83ae34a354c5e1a93ecda3b1531ffe5c4f
indrajith k.u <indrajithgamedevelouper2021@gmail.com>
2025-12-09 05:52:13 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,12 @@
 // This file is intentionally left blank.
-// The Firebase configuration will be populated by the backend.
 // You can find your web app's Firebase configuration in the Firebase console.
-export const firebaseConfig = {};
-
-    
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+export const firebaseConfig = {
+  apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "000000000000",
+  appId: "1:000000000000:web:0000000000000000000000",
+  measurementId: "G-XXXXXXXXXX"
+};

dd973946589c4c66d32ac9895ea690669d31d4b7
indrajith k.u <indrajithgamedevelouper2021@gmail.com>
2025-12-09 06:10:50 +0000

    pls issue a api key for sign in

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,13 @@
 // This file is intentionally left blank.
 // You can find your web app's Firebase configuration in the Firebase console.
 // See: https://firebase.google.com/docs/web/learn-more#config-object
+
+// 1. Go to your Firebase project console: https://console.firebase.google.com/
+// 2. In your project, go to Project Settings (the gear icon).
+// 3. In the "Your apps" card, select the web app for this project.
+// 4. Look for "SDK setup and configuration" and select "Config".
+// 5. Copy the entire `firebaseConfig` object and paste it below.
+
 export const firebaseConfig = {
   apiKey: "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
   authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
